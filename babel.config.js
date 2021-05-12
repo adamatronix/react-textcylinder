ae75b8816b0ca855a07743f9d548b98a56371387
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-05-12 17:20:20 +0800

    add class properties plugin to the babel config

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = function (api) {
   api.cache(true);
   
   const presets = [ "@babel/preset-env", "@babel/preset-react" ];
-  const plugins = ["inline-react-svg"];
+  const plugins = ["inline-react-svg", "@babel/plugin-proposal-class-properties"];
 
   return {
     presets,
